b351ba69448d29d0e4fe8731c23d23a461e993b4
SrPerso <carlos@jardiper.es>
2018-12-03 19:08:36 +0100

    Speedometter & starting tablet ui

---
+++ Assets/Ui/Parabrisas/Scripts/SpeedoMeter.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedoMeter : MonoBehaviour {
+
+    Image img;
+
+
+
+  
+    public Slider vel;//velocity
+    public Text speed_text;
+
+    private int initRotation = 120;
+
+
+	void Start ()
+    {
+        img = GetComponent<Image>();
+
+
+    }
+	
+
+	void Update ()
+    {
+
+       
+        img.rectTransform.eulerAngles = new Vector3(0, 0, initRotation -  vel.value);
+
+        speed_text.text = vel.value.ToString();
+
+    }
+
+
+}
