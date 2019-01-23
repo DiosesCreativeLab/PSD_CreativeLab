dfdca3dbebd7eeb3fa01e45f310f3be83936d513
SrPerso <carlos@jardiper.es>
2019-01-23 11:46:37 +0100

    Added  filters scene now correct
    bug to fix: the alpha picks 0.5016 not 0.5 then the if cant enter

--- Assets/Ui/Buttons_Manager.cs
+++ Assets/Ui/Buttons_Manager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Buttons_Manager : MonoBehaviour {
 
@@ -16,6 +17,25 @@ public class Buttons_Manager : MonoBehaviour {
         GO2.SetActive(false);
         GO3.SetActive(false);
 
+    }
+
+    public void changeAlpha(GameObject image)
+    {
+        float transparency = 0.5f; // (float)128 / (float)256;
+
+        if (true/*mathf.Round(image.GetComponent<Image>().color.a) == 0.5f*/)
+        {
+  
+
+            image.GetComponent<Image>().color = new Color32(233, 131, 131, 255);
+     
+        }
+        else
+        {
+            image.GetComponent<Image>().color = new Color32(233, 131, 131, 128);
+            //cb.a = transparency;
+            //cb2.a = transparency;
+        }
     }
 
 }
