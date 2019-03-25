9e4386b5fbdf544b7694559ab9ad3cd5c819ef27
SrPerso <carlos@jardiper.es>
2019-03-25 20:28:03 +0100

    commiteooo start

---
+++ Assets/Selector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour {
+
+    public Animator animator;
+    GameObject[] noselectedList;
+
+
+    public void SetAnimint(int integ)
+    {
+        animator.SetInteger("Anim_Int_Controller", integ);
+    }
+
+    //public void SelecttButton(GameObject selecteed)
+    //{
+    //    foreach(selecteed noselectedList)
+    //    {
+
+    //    }
+
+    //}
+
+
+
+}
