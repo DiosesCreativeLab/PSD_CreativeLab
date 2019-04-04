296612be649b9f2b64566cf99233152bae02140e
SrPerso <carlos@jardiper.es>
2019-04-04 17:04:44 +0200

    animations added, Retenciones WIP, añadido timer

---
+++ Assets/Timerr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timerr : MonoBehaviour
+{
+    public Text txt;
+    public Text txt2;
+    private float tiiime = 0;
+   
+    void Update()
+    {
+        tiiime += Time.deltaTime;
+        txt2.text = tiiime.ToString();
+        txt.text = Time.time.ToString();
+    }
+}
