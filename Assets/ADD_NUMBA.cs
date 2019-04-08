b975dc156d96ed49edfbeacace1f36e10ab7d46d
Guillermo Jesus Pinto Henriques <pintog96@gmail.com>
2019-04-09 00:23:03 +0200

    asd

---
+++ Assets/ADD_NUMBA.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ADD_NUMBA : MonoBehaviour
+{
+    public Text tx;
+    public int num;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            tx.text = num.ToString();
+        }
+    }
+}
