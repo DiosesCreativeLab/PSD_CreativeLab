de0d599ab51dc1219bde53f58cf22cfd6b9d9aa8
SrPerso <carlos@jardiper.es>
2019-04-08 20:23:41 +0200

    improved scenes and other

---
+++ Assets/scripts/mainControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mainControl : MonoBehaviour
+{
+    public static string PlayerName;
+    public Text Username_field;
+    public InputField InputFieldd;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerName = InputFieldd.text.ToString();
+    }
+
+    public string GetNamee()
+    {
+        return PlayerName;
+    }
+}
