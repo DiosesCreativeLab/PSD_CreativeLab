407889ffb1e378001e8073c011d36bd33f4b0aa6
SrPerso <carlos@jardiper.es>
2019-04-08 19:30:50 +0200

    to pass scenes

--- Assets/scripts/Variables_Controler.cs
+++ Assets/scripts/Variables_Controler.cs
@@ -73,7 +73,7 @@ public class Variables_Controler : MonoBehaviour
     {
 
         currentPlayer.timee += Time.deltaTime;
-        txt.text = currentPlayer.timee.ToString();
+        txt.text = currentPlayer.timee.ToString("n2");
 
     }
     // -----------
---
+++ Assets/toclose.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toclose : MonoBehaviour
+{
+    public Variables_Controler vc;
+
+
+    void OnTriggerEnter(Collider Other_col)
+    {
+
+        Debug.Log("trigered");
+
+        if (Other_col.tag == "Player")
+        {
+            vc.changeScene(0);
+        }
+    }
+
+}
